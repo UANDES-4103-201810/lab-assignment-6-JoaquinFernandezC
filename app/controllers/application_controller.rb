d04d4478b0c713f1949dd5b44f93fa1b9d4af6e3
JoaquinFernandezC <jafernandez1@miuandes.cl>
2018-04-17 15:22:07 -0300

    Modified Application controler. Now shows user path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def is_user_logged_in?
     #complete this method
     !!current_user
-    logged_in = false
-    if logged_in then true else redirect_to root_path end
+    #logged_in = false
+    #if logged_in then true else redirect_to root_path end
   end
 end
